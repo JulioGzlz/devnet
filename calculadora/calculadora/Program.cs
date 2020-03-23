698ad3ea2259dfa76722bf7e076d96de1d21f1d8
Julio Cesar Gonzalez Gonzalez <jcesargon7@gmail.com>
2020-03-22 19:38:04 -0600

    Examen en c#

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -5,8 +5,18 @@ namespace calculadora
     class Program
     {
         static void Main(string[] args)
+            
+
         {
-            Console.WriteLine("Hello World!");
+            double a, b, resul;
+            Console.WriteLine("Ingrese su primer digito ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su segundo digito ");
+            b = double.Parse(Console.ReadLine());
+
+            resul = a + b;
+
+            Console.WriteLine("El resultado de su operacion es: "+resul);
         }
     }
 }
